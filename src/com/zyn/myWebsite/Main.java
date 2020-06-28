5b1d4887ee571b61d79e73a3d7ee1fd7167ec724
YAANNZ <zhuynchn@163.com>
2020-06-28 22:22:27 +0800

    628

--- src/com/zyn/myWebsite/Main.java
+++ src/com/zyn/myWebsite/Main.java
@@ -1,9 +1,19 @@
 package com.zyn.myWebsite;
 
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("123");
+        for (int i = 0; i < 5; i++) {
+            for (int i1 = 0; i1 < 5; i1++) {
+                System.out.print("*");
+            }
+            System.out.println();
+//            System.out.print("\n");
+        }
+
+
     }
+
 }
